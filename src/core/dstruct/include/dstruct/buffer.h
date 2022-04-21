2f9c38bcebeb3fd2952ad3591bbb1566d1fd6105
Onur Sertkaya <sertkaya93@gmail.com>
2022-04-21 09:14:25 +0200

    Refine StaticBuffer and DynamicBuffer
    - Fix const/non-const member functions for correct use
    - Add static asserts for not accepting const template parameters
      (behavior adapted after STL containers)
    - Expand tests in a naive way to address the correctness of the
      above

--- src/core/dstruct/include/dstruct/buffer.h
+++ src/core/dstruct/include/dstruct/buffer.h
@@ -9,16 +9,64 @@ namespace kioku
 namespace core
 {
 
+// Generic contiguous data buffer with fixed size.
+template <typename T, std::size_t N>
+class StaticBuffer
+{
+    static_assert(N > 0U, "StaticBuffer must have a positive size.");
+    static_assert(!std::is_const_v<T>, "StaticBuffer should have a non-const type. Constness must "
+                                       "be achieved by marking the variable const.");
+
+ public:
+    using value_type = T;
+
+    StaticBuffer() = default;
+    ~StaticBuffer() = default;
+
+    StaticBuffer(StaticBuffer&) = delete;
+    StaticBuffer(StaticBuffer&&) = delete;
+    StaticBuffer& operator=(StaticBuffer&) = delete;
+    StaticBuffer& operator=(StaticBuffer&&) = delete;
+
+    // Variadic ctor.
+    template <typename... Ts, std::size_t NumTs = sizeof...(Ts),
+              std::enable_if_t<(std::is_same_v<Ts, T> && ...) && (NumTs > 1U), bool> = true>
+    explicit StaticBuffer(Ts&&... elems) :
+        data_{elems...}
+    {
+    }
+
+    T* data() { return data_; }
+    T const* data() const { return data_; }
+
+    T& operator[](std::size_t const idx) { return data_[idx]; }
+    T operator[](std::size_t const idx) const { return data_[idx]; }
+
+    [[nodiscard]] std::size_t size() const { return size_; }
+
+ private:
+    static constexpr std::size_t size_{N};
+    T data_[N]{};
+};
+
 // Generic contiguous data buffer with adjustable size.
 template <typename T>
 class DynamicBuffer
 {
+    static_assert(!std::is_const_v<T>, "DynamicBuffer should have a non-const type. Constness must "
+                                       "be achieved by marking the variable const.");
+
  public:
     using value_type = T;
 
     explicit DynamicBuffer(std::size_t const size) :
         size_{size}
     {
+        // TODO:
+        // It does not make sense to create a const adjustable buffer through this ctor.
+        // find a way to detect and prevent at compile time, either with a working version of the
+        // following or a factory function.
+        // static_assert(!std::is_const_v<decltype(*this)>, "");
         KIOKU_ASSERT(size_ > 0U);
         ptr_ = new T[size_]{};
     }
@@ -43,8 +91,10 @@ class DynamicBuffer
     ~DynamicBuffer() { delete[] ptr_; }
 
     T* data() { return ptr_; }
+    T const* data() const { return ptr_; }
 
     T& operator[](std::size_t const idx) { return ptr_[idx]; }
+    T operator[](std::size_t const idx) const { return ptr_[idx]; }
 
     [[nodiscard]] std::size_t size() const { return size_; }
 
@@ -74,42 +124,6 @@ class DynamicBuffer
     T* begin() { return ptr_; }
 
     T* end() { return ptr_ + static_cast<std::ptrdiff_t>(size_); }
-};
-
-// Generic contiguous data buffer with fixed size.
-template <typename T, std::size_t N>
-class StaticBuffer
-{
-    static_assert(N > 0U, "Static buffer must have a positive size.");
-
- public:
-    using value_type = T;
-
-    StaticBuffer() = default;
-    ~StaticBuffer() = default;
-
-    StaticBuffer(StaticBuffer&) = delete;
-    StaticBuffer(StaticBuffer&&) = delete;
-    StaticBuffer& operator=(StaticBuffer&) = delete;
-    StaticBuffer& operator=(StaticBuffer&&) = delete;
-
-    // Variadic ctor.
-    template <typename... Ts, std::size_t NumTs = sizeof...(Ts),
-              std::enable_if_t<(std::is_same_v<Ts, T> && ...) && (NumTs > 1U), bool> = true>
-    explicit StaticBuffer(Ts&&... elems) :
-        data_{elems...}
-    {
-    }
-
-    T* data() { return data_; }
-
-    T& operator[](std::size_t const idx) { return data_[idx]; }
-
-    [[nodiscard]] std::size_t size() const { return size_; }
-
- private:
-    static constexpr std::size_t size_{N};
-    T data_[N]{};
 };
 
 }  // namespace core
--- src/core/dstruct/test/test_buffer.cpp
+++ src/core/dstruct/test/test_buffer.cpp
@@ -6,6 +6,56 @@
 using kioku::core::DynamicBuffer;
 using kioku::core::StaticBuffer;
 
+// TODO: find a more elegant solution to write the same tests for const instances.
+// possibly via  TYPED_TEST.
+
+TEST(StaticBuffer, create)
+{
+    auto b = StaticBuffer<int, 10U>{};
+    ASSERT_EQ(b.size(), 10U);
+
+    auto const c_b = StaticBuffer<int, 5U>{};
+    ASSERT_EQ(c_b.size(), 5U);
+}
+
+TEST(StaticBuffer, variadic_create)
+{
+    auto b = StaticBuffer<int, 10U>{5, 6, 7, 8, 1, 2, 3, 4};
+    ASSERT_EQ(b.size(), 10U);
+
+    ASSERT_EQ(b[0U], 5);
+    ASSERT_EQ(b[1U], 6);
+    ASSERT_EQ(b[2U], 7);
+    ASSERT_EQ(b[3U], 8);
+    ASSERT_EQ(b[4U], 1);
+    ASSERT_EQ(b[5U], 2);
+    ASSERT_EQ(b[6U], 3);
+    ASSERT_EQ(b[7U], 4);
+
+    ASSERT_EQ(b[8U], int{});
+    ASSERT_EQ(b[9U], int{});
+
+    auto const c_b = StaticBuffer<int, 5U>{5, 6, 1, 2};
+    ASSERT_EQ(c_b.size(), 5U);
+
+    ASSERT_EQ(c_b[0U], 5);
+    ASSERT_EQ(c_b[1U], 6);
+    ASSERT_EQ(c_b[2U], 1);
+    ASSERT_EQ(c_b[3U], 2);
+
+    ASSERT_EQ(c_b[4U], int{});
+}
+
+TEST(StaticBuffer, index_access)
+{
+    auto b = StaticBuffer<int, 4U>{};
+    b[0U] = 10;
+    b[1U] = 20;
+
+    ASSERT_EQ(b[0U], 10);
+    ASSERT_EQ(b[1U], 20);
+}
+
 TEST(DynamicBuffer, create)
 {
     auto b = DynamicBuffer<int>{10};
@@ -14,13 +64,41 @@ TEST(DynamicBuffer, create)
 
 TEST(DynamicBuffer, create_from_container)
 {
-    auto b = DynamicBuffer<int>{std::array<int, 4>{4, 3, 2, 1}};
-    ASSERT_EQ(b.size(), 4U);
+    // from rvalue
+    auto b_from_rvalue = DynamicBuffer<int>{std::array<int, 4>{4, 3, 2, 1}};
+    ASSERT_EQ(b_from_rvalue.size(), 4U);
 
-    b[0U] = 4;
-    b[1U] = 3;
-    b[2U] = 2;
-    b[3U] = 1;
+    ASSERT_EQ(b_from_rvalue[0U], 4);
+    ASSERT_EQ(b_from_rvalue[1U], 3);
+    ASSERT_EQ(b_from_rvalue[2U], 2);
+    ASSERT_EQ(b_from_rvalue[3U], 1);
+
+    // from non-const lvalue
+    std::array<int, 4U> some_array{5, 6, 7};
+    auto b_from_lvalue = DynamicBuffer<int>{some_array};
+
+    ASSERT_EQ(b_from_lvalue[0U], 5);
+    ASSERT_EQ(b_from_lvalue[1U], 6);
+    ASSERT_EQ(b_from_lvalue[2U], 7);
+    ASSERT_EQ(b_from_lvalue[3U], int{});
+
+    // from const lvalue
+    std::array<int, 4U> const some_const_array{1, 2, 3};
+    auto b_from_const_lvalue = DynamicBuffer<int>{some_const_array};
+
+    ASSERT_EQ(b_from_const_lvalue[0U], 1);
+    ASSERT_EQ(b_from_const_lvalue[1U], 2);
+    ASSERT_EQ(b_from_const_lvalue[2U], 3);
+    ASSERT_EQ(b_from_const_lvalue[3U], int{});
+
+    // const instance from rvalue
+    auto const c_b = DynamicBuffer<int>{std::array<int, 4>{4, 3, 2, 1}};
+    ASSERT_EQ(c_b.size(), 4U);
+
+    ASSERT_EQ(c_b[0U], 4);
+    ASSERT_EQ(c_b[1U], 3);
+    ASSERT_EQ(c_b[2U], 2);
+    ASSERT_EQ(c_b[3U], 1);
 }
 
 TEST(DynamicBuffer_DeathTest, create_assert)
@@ -40,6 +118,11 @@ TEST(DynamicBuffer, indexed_access)
     ASSERT_EQ(b[1U], 20);
     ASSERT_EQ(b[2U], 30);
     ASSERT_EQ(b[3U], 40);
+
+    // TODO: this ctor does not make sense as the created object
+    // does not support further index assignment. see the ctor
+    // for details
+    auto const c_b = DynamicBuffer<int>{3};
 }
 
 TEST(DynamicBuffer, reallocation)
@@ -70,40 +153,9 @@ TEST(DynamicBuffer_DeathTest, reallocation_assert)
     ASSERT_DEATH({ static_cast<void>(b.reallocate(2U)); }, ".*");
 }
 
-TEST(StaticBuffer, create)
+TEST(DynamicBuffer_DeathTest, reallocation_assert_2)
 {
-    auto b = StaticBuffer<int, 10U>{};
-    ASSERT_EQ(b.size(), 10U);
-}
+    auto b = DynamicBuffer<int>{2};
 
-TEST(StaticBuffer, variadic_create)
-{
-    auto b = StaticBuffer<int, 10U>{5, 6, 7, 8, 1, 2, 3, 4};
-    ASSERT_EQ(b.size(), 10U);
-
-    ASSERT_EQ(b[0U], 5);
-    ASSERT_EQ(b[1U], 6);
-    ASSERT_EQ(b[2U], 7);
-    ASSERT_EQ(b[3U], 8);
-    ASSERT_EQ(b[4U], 1);
-    ASSERT_EQ(b[5U], 2);
-    ASSERT_EQ(b[6U], 3);
-    ASSERT_EQ(b[7U], 4);
-
-    ASSERT_EQ(b[8U], int{});
-    ASSERT_EQ(b[9U], int{});
-}
-
-TEST(StaticBuffer, indexed_access)
-{
-    auto b = StaticBuffer<int, 4U>{};
-    b[0U] = 10;
-    b[1U] = 20;
-    b[2U] = 30;
-    b[3U] = 40;
-
-    ASSERT_EQ(b[0U], 10);
-    ASSERT_EQ(b[1U], 20);
-    ASSERT_EQ(b[2U], 30);
-    ASSERT_EQ(b[3U], 40);
+    ASSERT_DEATH({ static_cast<void>(b.reallocate(2U)); }, ".*");
 }
